e849d2be566ce06f1c05f32c89a755f1567958a1
Vaibhav <vaibhav@parcelhero.in>
2023-10-16 19:27:04 +0530

    Created Publisher and COnsumer

---
+++ Publisher/Controllers/PublishController.cs
@@ -0,0 +1,26 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+
+namespace Publisher.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublishController : ControllerBase
+    {
+        private readonly IPublishEndpoint _publisher;
+
+        public PublishController(IPublishEndpoint _publisher)
+        {
+            this._publisher = _publisher;
+        }
+        [HttpGet]
+        public async Task<string> PublishMessage()
+        {
+            await _publisher.Publish(new Order { Name = "Vaibhav " + Guid.NewGuid().ToString() });
+
+            return "Success";
+        }
+    }
+}
